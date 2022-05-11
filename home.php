3ac3a10db43bb70e9afb02c3ac75ad74a16f5d64
João Victor Mendes <joao.mendes.costa97@gmail.com>
2022-05-10 23:51:03 -0500

    Update home.php

--- home.php
+++ home.php
@@ -23,7 +23,7 @@
       </a>
       <ul class="navbar-nav">
           <li class="nav-item">
-            <a href="logoff.php" class="nav-link">SAIR</a>
+            <a href="logoff.php" class="nav-link">SAIiR</a>
           </li>
       </ul>
     </nav>
